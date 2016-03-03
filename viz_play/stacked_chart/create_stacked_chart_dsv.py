1c6ee873fdbccfb833f09cb85a38fd9f14a5cb2e
Fletcher Hazlehurst <fletcher.haz@gmail.com>
2016-03-03 14:36:42 -0500

    Added stacked chart of top ten categories

---
+++ viz_play/stacked_chart/create_stacked_chart_dsv.py
@@ -0,0 +1,27 @@
+import json
+
+from operator import itemgetter
+
+with open('categories_decades_transformed.json', 'r') as f:
+  categories = json.load(f)
+
+years = range(1000, 2011, 50)
+
+f = open('categories.dsv', 'w')
+
+cats = categories.keys()
+
+order = sorted(zip(categories.keys(), [categories[category]['2000'] for category in categories.keys()]), key=itemgetter(1), reverse=True)
+
+print order
+
+f.write('key#value#date\n')
+
+for i in range(0, 10):
+  category = order[i][0]
+  for year in years:
+    if not str(year) in categories[category]:
+      val = '0'
+    else:
+      val =  str(categories[category][str(year)])
+    f.write(category + '#' + val + '#' + str(year) + '\n')
