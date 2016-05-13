9c348edc31f6d1152a19beb6b2b907969f7d8b15
Fletcher Hazlehurst <fletcher.haz@gmail.com>
2016-05-13 09:27:29 -0400

    CSV data and script to create it

--- src/tools/json_to_csv.py
+++ src/tools/json_to_csv.py
@@ -1,9 +1,15 @@
 import json
+import random
 
-path = "~/Downloads/Entertainment.json"
-with open(path, 'r') as f:
-  a = json.load(f)
+categories = ['The_Arts', 'Relative_position', 'Sport', 'Farming', 'Navigation', 'Direction', 'Legal_possession', 'Animals_collectively', 'Trading_organization', 'People_collectively']
 
-with open('../Entertainment.csv', 'w') as f:
-  for x in a:
-    f.write(str(x[0]) + ',' + str(x[1]) + '\n')
+for category in categories:
+  with open('../viz_play/rankings/articles_for_tags/' + category + '.json', 'r') as f:
+    a = json.load(f)
+
+  with open('../data/csv/' + category + '.csv', 'w') as f:
+    print category, len(a)
+    sample = random.sample(xrange(len(a)), 100)
+    for s in sample:
+      x = a[s]
+      f.write(str(x[0].encode('utf-8')) + '\t' + str(x[1]) + '\n')
